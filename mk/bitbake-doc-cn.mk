dfaa98315f4005ace2c332089604e27328bc0a32
George Wang <gwang@embednix.com>
2024-06-07 00:24:26 -0700

    update to ver 2.8

--- mk/bitbake-doc-cn.mk
+++ mk/bitbake-doc-cn.mk
@@ -1,13 +1,14 @@
 BITBAKE_DOC_CN_PAGES = \
 	0_cover_title/cover_title.tex \
-	5_variables_glossary/variables_glossary.tex \
+	6_variables_glossary/variables_glossary.tex \
 	0_cover_title/begin_document.tex \
 	0_cover_title/table_of_contents.tex \
-	0_cover_title/copyright.tex \
+	0_cover_title/preface.tex \
 	1_overview/overview.tex \
 	2_execution/execution.tex	\
 	3_syntax_and_operators/syntax_and_operators.tex \
-	4_file_download_support/file_download_support.tex \
-	6_hello_world_example/hello_world_example.tex \
-	7_index/variable_index.tex \
+	4_variable_context/variable_context.tex \
+	5_file_download_support/file_download_support.tex \
+	7_hello_world_example/hello_world_example.tex \
+	8_index/variable_index.tex \
 	0_cover_title/last_page.tex
